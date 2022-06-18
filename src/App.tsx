25143c1959c55560a6fd2dc9b75914b9baae966a
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2022-06-18 13:07:29 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { Card } from "./components/Card";
+import styled from "styled-components";
+import img1 from "./Assets/images/img1.jpg";
+import img2 from "./Assets/images/img2.jpg";
+
+const CardContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #1f2229;
+  overflow: hidden;
+`;
+
+const Separator = styled.span`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Card component</h1>
+      <h2>props: title, date, and img</h2>
+      <CardContainer>
+        <Card title={"hello world"} date={1} imgUrl={img1} />
+        <Separator />
+        <Card title={"My Card"} date={2} imgUrl={img2} />
+      </CardContainer>
+    </div>
+  );
+}
